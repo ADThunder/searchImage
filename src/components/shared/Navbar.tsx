b651190df5d6748565cda79da4a0d001a17c27a3
ADThunder <maianhdung02051996@gmail.com>
2024-02-19 04:23:55 +0700

    first commit

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <div className="max-w-7xl mx-auto py-4 w-full">
+      {/* logo */}
+      <h1 className="text-3xl">Image Gallery</h1>
+    </div>
+  );
+}
